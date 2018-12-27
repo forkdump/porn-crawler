8689763043e4302b13130c3d6da9552c7a202a77
Roger <sunrimii@gmail.com>
2018-12-27 20:04:35 +0800

    fixed some bugs

--- website/crawler/static/vendor/test.js
+++ website/crawler/static/vendor/test.js
@@ -1,14 +1,5 @@
 function blur_switch()
 {
-    if (document.getElementById('switch').checked)
-        for (let index = 0; index < document.getElementsByName('video').length; index++) {
-            const element = document.getElementsByName('video')[index];
-            element.className = 'card-body blur';
-        }
-    else
-        for (let index = 0; index < document.getElementsByName('video').length; index++) {
-            const element = document.getElementsByName('video')[index];
-            element.className = 'card-body';
-        }
-    }
-
+    document.getElementsByName('video').forEach
+    (element => element.className = document.getElementById('switch').checked ? 'card-body blur' : 'card-body')
+}
